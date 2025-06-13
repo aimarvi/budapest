dc6b6c9e3b092ee760822e4ab20ab83a5c56ecaa
Ammar Marvi <ammar.marvi01@gmail.com>
2025-06-13 18:48:48 -0400

    low-level trigger

--- scripts/presentation/input_utils.py
+++ scripts/presentation/input_utils.py
@@ -1,4 +1,6 @@
 import time
+import os
+import fcntl
 
 def read_trigger(dev='/dev/hidraw1'):
     with open(dev, 'rb') as f:
@@ -30,8 +32,21 @@ def check_trigger_continuous(f):
         pass  # no data available
     return False
 
+def check_trigger_fd(fd):
+    try:
+        data = os.read(fd, 8)
+        if data and data[0] != 0:
+            return True
+    except BlockingIOError:
+        pass  # No data available
+    return False
+
 if __name__=='__main__':
-    while not read_trigger():
-        continue
+    # Open the device in read-only mode
+    fd = os.open('/dev/hidraw1', os.O_RDONLY)
+    
+    # Set the file descriptor to non-blocking
+    flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
 
     print('yay!')
